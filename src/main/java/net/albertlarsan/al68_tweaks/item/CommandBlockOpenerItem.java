96b2e3da069de14683181290cda8d000ecc338e5
AlbertLarsan68 <74931857+albertlarsan68@users.noreply.github.com>
2021-10-29 20:41:32 +0200

    Remove log entries in CommandBlockOpenerItem

--- src/main/java/net/albertlarsan/al68_tweaks/item/CommandBlockOpenerItem.java
+++ src/main/java/net/albertlarsan/al68_tweaks/item/CommandBlockOpenerItem.java
@@ -1,6 +1,5 @@
 package net.albertlarsan.al68_tweaks.item;
 
-import net.albertlarsan.al68_tweaks.AL68Tweaks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.Item;
@@ -19,11 +18,6 @@ public class CommandBlockOpenerItem extends Item {
         if (clickedBlockEntity.getType() == BlockEntityType.COMMAND_BLOCK) {
             context.getPlayer().openCommandBlockScreen(context.getWorld()
                     .getBlockEntity(context.getBlockPos(), BlockEntityType.COMMAND_BLOCK).orElseThrow());
-            if (context.getWorld().isClient()) {
-                AL68Tweaks.LOGGER.info("Client");
-            } else {
-                AL68Tweaks.LOGGER.info("Server");
-            }
             return ActionResult.SUCCESS;
         }
 
